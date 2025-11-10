a498bb9290906bfbe7a9025de7cb88fe296b4e3b
cooper8k <orlanprado.c@gmail.com>
2025-11-10 14:27:39 -0300

    arreglando feing

--- src/main/java/com/huerto_hogar_e3/pedidos/PedidosApplication.java
+++ src/main/java/com/huerto_hogar_e3/pedidos/PedidosApplication.java
@@ -2,10 +2,10 @@ package com.huerto_hogar_e3.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@FeignClient
+@EnableFeignClients
 public class PedidosApplication {
 
 	public static void main(String[] args) {
